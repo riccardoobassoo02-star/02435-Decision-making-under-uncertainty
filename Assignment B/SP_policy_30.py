927c1d5c1a21bc27e3af7c0e35a17dda5178f012
Riccardo Basso <s253206@dtu.dk>
2026-04-11 16:07:11 +0200

    big values m fixed

--- Assignment B/SP_policy_30.py
+++ Assignment B/SP_policy_30.py
@@ -32,7 +32,7 @@ eta_vent    = data['humidity_vent_coeff']
 min_up_time = data['vent_min_up_time']
 
 M_temp = 50  # big-M constant for temperature ()
-M_hum = 150   # big-M constant for humidity
+M_hum = 100   # big-M constant for humidity
 epsilon = 0.1 # small constant for strict inequalities for overrule controllers 
 
 # Note: initial conditions (T0, H0) are not extracted here because they are provided at runtime by the environment via the state dictionary 
