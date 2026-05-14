c5d1f0f0fd20ba6938273df18bcb6b4f53b93bc2
Gerard <gerri7003@gmail.com>
2026-05-14 16:22:04 +0200

    including overrulers logic

--- Assignment B/Policies/DUMMY_policy_30.py
+++ Assignment B/Policies/DUMMY_policy_30.py
@@ -1,10 +1,39 @@
-### DUMMY POLICY
-# never turns on the ventilation nor any heater, and leaves everything up to the overrule controllers
+from Utils.v2_SystemCharacteristics import get_fixed_data
+
+# Parameters extraction from system characteristics
+data = get_fixed_data()
+HEATING_MAX_POWER = data["heating_max_power"]
+VENT_MIN_UP_TIME  = data["vent_min_up_time"]
+
 
 def select_action(state):    
+    # Override controllers state
+    low_override_r1 = state["low_override_r1"]
+    low_override_r2 = state["low_override_r2"]
+
+    # current humidity and consecutive ventilation counter
+    humidity = state["H"]
+    vent_counter = state["vent_counter"]
+
+
+    # Never turns on the ventilation nor any heater, unless is forced by the overrule controllers
+    p1 = (0 if not low_override_r1
+            else HEATING_MAX_POWER
+    )
+
+    p2 = (0 if not low_override_r2
+            else HEATING_MAX_POWER
+    )
+
+    v = (
+        1 if (humidity > data["humidity_threshold"]) or (0 < vent_counter < VENT_MIN_UP_TIME)
+        else 0
+    )
+
+
     HereAndNowActions = {
-    "HeatPowerRoom1" : 0,
-    "HeatPowerRoom2" : 0,
-    "VentilationON" : 0
+    "HeatPowerRoom1" : p1,
+    "HeatPowerRoom2" : p2,
+    "VentilationON" : v
     }
     return HereAndNowActions
